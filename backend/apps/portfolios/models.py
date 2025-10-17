04f079fb97965db4e474c756bff9b370558959c9
Ícaro Gabryel <icarogabryel2001@gmail.comm>
2025-10-16 21:48:07 -0300

    fix: add comment for type hinting in Portfolio model's id field

--- backend/apps/portfolios/models.py
+++ backend/apps/portfolios/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Portfolio(models.Model):
-    id: models.AutoField
+    id: models.AutoField  # For type hinting purposes
     name = models.CharField(max_length=100)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
